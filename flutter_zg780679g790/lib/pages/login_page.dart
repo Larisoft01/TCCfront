3d9f265072b08089cc25ac45ba0db80d1997d909
Larisoft <81933900+Larisoft01@users.noreply.github.com>
2025-10-03 18:43:37 -0300

    Add files via upload

---
+++ flutter_zg780679g790/lib/pages/login_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart' show BuildContext, StatelessWidget, Widget;
+import '../design/login_design.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginDesign();
+  }
+}
---
+++ flutter_zg780679g790/lib/pages/perfil_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PerfilPage extends StatelessWidget {
+  const PerfilPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Perfil")),
+      body: const Center(
+        child: Text(
+          "Página de Perfil",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
